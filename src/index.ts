3291be56d01825d9181175f725c6086fad9d0e63
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2024-03-13 15:48:13 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
   config();
 }
 
-const server = http.createServer(app); 
+// const server = http.createServer(app); 
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () =>
+app.listen(port, () =>
   console.log(`API available on http://localhost:${port}`)
 );
