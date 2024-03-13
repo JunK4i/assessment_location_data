929b33070824858714895252ba5577f9186282bb
Koh Jun Kai <49807837+JunK4i@users.noreply.github.com>
2024-03-13 17:11:34 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,19 @@ export const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: `uknonwn endpoint` });
   };
   
-export const decryptData = (encryptedDataWithIv: Buffer, key: Buffer): string | null  => {
+export const decryptData = (encryptedDataWithIvAndTag: Buffer, key: Buffer): string | null  => {
     const IV_LENGTH = 12; // For AES-256 GCM
+    const TAG_LENGTH = 16; // 128 bits for the authentication tag
+
     try {
-        const iv = encryptedDataWithIv.subarray(0, IV_LENGTH);
+        const iv = encryptedDataWithIvAndTag.subarray(0, IV_LENGTH);
+        const tag = encryptedDataWithIvAndTag.subarray(encryptedDataWithIvAndTag.length - TAG_LENGTH);
+        const encryptedData = encryptedDataWithIvAndTag.subarray(IV_LENGTH, encryptedDataWithIvAndTag.length - TAG_LENGTH);
         console.log("iv:", iv, iv.length);
         console.log("key", key, key.length);
-        const encryptedData = encryptedDataWithIv.subarray(IV_LENGTH);
         console.log("encryptedData:", encryptedData);
         const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv);
+        decipher.setAuthTag(tag); 
         let decrypted = decipher.update(encryptedData);
         decrypted = Buffer.concat([decrypted, decipher.final()]);
         console.log("decrypted:", decrypted);
